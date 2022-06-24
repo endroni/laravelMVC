cfd03dfc63a06290d466dc6cb945c639080dda60
endroni <rubao.rcs@gmail.com>
2022-06-24 10:48:38 -0300

    rotas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function(){
     return view('welcome');
 });
+
+Route::get('/teste', function() {
+    return "<b>Isto é um <i>itálico</i></br>";
+});
+                                                                    //Expressão regular
+Route::get('/teste/{id}', fn($id) => "Teste $id" ) ->where ('id', '[0-9]{1,8}'); 
+
+Route::domain('{sub}.localhost')->group(
+    function(){
+        Route::get('/idioma/{id}', function($sub, $id){
+            if($sub == 'en'){
+                return "Hello world $id";
+            }
+            else {
+                return "Olá mundo $id";
+            }
+        });
+
+        Route::get('/outra-rota', fn()=>"Outra rota");
+    }
+);
